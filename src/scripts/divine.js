f649460a488bbbd870c3b47856ac4793a48398e7
carrie <popo950061@gmail.com>
2023-08-20 13:11:38 +0800

    update

--- src/scripts/divine.js
+++ src/scripts/divine.js
@@ -1,18 +1,34 @@
 import 'divine.css';
 import { createApp } from 'petite-vue';
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, EffectCoverflow } from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation],
-  slidesPerView: 5,
-  spaceBetween: 30
-});
+// Swiper.use([Navigation, Pagination, Scrollbar, EffectCoverflow]);
+
+let windowWidth = window.innerWidth;
+
+if (windowWidth < 750) {
+  const swiper = new Swiper('.swiper', {
+    // modules: [Navigation, EffectCoverflow],
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true
+    // loopAdditionalSlides: 30
+    // effect: 'coverflow',
+    // coverflowEffect: {
+    //   rotate: -30,
+    //   stretch: 10,
+    //   depth: 100,
+    //   modifier: 1,
+    //   slideShadows: true
+    // }
+  });
+}
 
 const requireAll = (requireContext) => requireContext.keys().map(requireContext);
 const req = require.context('_svg/', false, /\.svg$/);
